8924f3a08d9b491a21f8a718a01a4c3d629183ff
manoelsmoraes <manoel@msminfo.com.br>
2025-04-23 12:07:59 -0300

    ok

--- detector-borda.v
+++ detector-borda.v
@@ -1,10 +1,34 @@
 module detector_borda (
-    input clk,
-    input rst,
-    input [1:0] entrada,
-    output [1:0] detector
+    input wire clk,
+    input wire rst,
+    input wire [1:0] entrada,
+    output wire [1:0] detector
 );
     
 //insira seu código aqui
 
+       reg [1:0] atual, anterior;
+       
+
+       // Lógica de detecção de borda de subida.
+
+       wire [1:0] borda;
+       assign borda = ~anterior & atual;
+       assign detector = borda;
+
+       //Bloco sequencial com reset assincrono
+
+       always @(posedge clk or posedge rst) begin
+           if (rst) begin
+                anterior <= 2'b00;
+                atual <= 2'b00;
+
+            end else begin
+
+                atual <= entrada;
+		anterior <= atual;
+
+           end
+
+   end
 endmodule
